56301ecdcc92f5867d0dc29bf69768102cd7c281
hokten <hhoohh2001@yahoo.com>
2025-12-11 09:37:49 +0200

    Create veritabani.php
    veritabani.php eklendi.

---
+++ veritabani.php
@@ -0,0 +1,9 @@
+<?php
+try {
+		$baglanti = new PDO("mysql:host=localhost;dbname=kursvt", "root", "123456");
+		$baglanti->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+	} catch (PDOException $e) {
+		print "Error: " . $e->getMessage();
+	}
+?>	
