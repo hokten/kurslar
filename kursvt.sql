a9fc5bf71a109e17b1a62309ff6bdef1b7050f38
hokten <hhoohh2001@yahoo.com>
2025-12-18 08:07:32 +0200

    Update kursvt.sql

--- kursvt.sql
+++ kursvt.sql
@@ -2,7 +2,7 @@
 -- Sunucu:                       127.0.0.1
 -- Sunucu sürümü:                8.0.36 - MySQL Community Server - GPL
 -- Sunucu İşletim Sistemi:       Win64
--- HeidiSQL Sürüm:               12.8.0.6908
+-- HeidiSQL Sürüm:               12.13.0.7147
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -16,71 +16,86 @@
 
 
 -- kursvt için veritabanı yapısı dökülüyor
+DROP DATABASE IF EXISTS `kursvt`;
 CREATE DATABASE IF NOT EXISTS `kursvt` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_turkish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `kursvt`;
 
--- tablo yapısı dökülüyor kursvt.kayitlar
-CREATE TABLE IF NOT EXISTS `kayitlar` (
+-- tablo yapısı dökülüyor kursvt.kullanicilar
+DROP TABLE IF EXISTS `kullanicilar`;
+CREATE TABLE IF NOT EXISTS `kullanicilar` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `kullanici_adi` varchar(50) COLLATE utf8mb4_turkish_ci NOT NULL DEFAULT '0',
+  `kullanici_sifresi` varchar(50) COLLATE utf8mb4_turkish_ci NOT NULL DEFAULT '0',
+  `kullanici_rolu` int NOT NULL DEFAULT (0),
+  PRIMARY KEY (`id`),
+  KEY `FK__roller` (`kullanici_rolu`),
+  CONSTRAINT `FK__roller` FOREIGN KEY (`kullanici_rolu`) REFERENCES `roller` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
+
+-- kursvt.kullanicilar: ~3 rows (yaklaşık) tablosu için veriler indiriliyor
+INSERT INTO `kullanicilar` (`id`, `kullanici_adi`, `kullanici_sifresi`, `kullanici_rolu`) VALUES
+	(1, 'admin', '123456', 1),
+	(2, 'ahmethoca', '5678', 2),
+	(3, 'ali', 'xyz', 3);
+
+-- tablo yapısı dökülüyor kursvt.kurs_egitmen
+DROP TABLE IF EXISTS `kurs_egitmen`;
+CREATE TABLE IF NOT EXISTS `kurs_egitmen` (
   `id` int NOT NULL AUTO_INCREMENT,
   `kurs_id` int DEFAULT '0',
   `egitmen_id` int DEFAULT '0',
   PRIMARY KEY (`id`),
-  KEY `FK3` (`kurs_id`),
-  KEY `FK4` (`egitmen_id`),
-  CONSTRAINT `FK3` FOREIGN KEY (`kurs_id`) REFERENCES `kurslar` (`id`),
-  CONSTRAINT `FK4` FOREIGN KEY (`egitmen_id`) REFERENCES `kullanicilar` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
+  KEY `FK__kurslar` (`kurs_id`),
+  KEY `FK__kullanicilar` (`egitmen_id`),
+  CONSTRAINT `FK__kullanicilar` FOREIGN KEY (`egitmen_id`) REFERENCES `kullanicilar` (`id`),
+  CONSTRAINT `FK__kurslar` FOREIGN KEY (`kurs_id`) REFERENCES `kurslar` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
 
--- kursvt.kayitlar: ~3 rows (yaklaşık) tablosu için veriler indiriliyor
-INSERT INTO `kayitlar` (`id`, `kurs_id`, `egitmen_id`) VALUES
-	(2, 2, 4),
-	(4, 2, 5),
-	(5, 1, 4);
+-- kursvt.kurs_egitmen: ~1 rows (yaklaşık) tablosu için veriler indiriliyor
+INSERT INTO `kurs_egitmen` (`id`, `kurs_id`, `egitmen_id`) VALUES
+	(1, 1, 2);
 
--- tablo yapısı dökülüyor kursvt.kullanicilar
-CREATE TABLE IF NOT EXISTS `kullanicilar` (
+-- tablo yapısı dökülüyor kursvt.kurs_ogrenci
+DROP TABLE IF EXISTS `kurs_ogrenci`;
+CREATE TABLE IF NOT EXISTS `kurs_ogrenci` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `kullanici_adi` varchar(100) COLLATE utf8mb4_turkish_ci DEFAULT NULL,
-  `kullanici_sifre` varchar(50) COLLATE utf8mb4_turkish_ci DEFAULT NULL,
-  `rol_id` int DEFAULT NULL,
+  `kurs_id` int NOT NULL,
+  `kursiyer_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_kullanicilar_roller` (`rol_id`),
-  CONSTRAINT `FK_kullanicilar_roller` FOREIGN KEY (`rol_id`) REFERENCES `roller` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
+  KEY `FKy__kurslar` (`kurs_id`),
+  KEY `FKy__kullanicilar` (`kursiyer_id`),
+  CONSTRAINT `FKy__kullanicilar` FOREIGN KEY (`kursiyer_id`) REFERENCES `kullanicilar` (`id`),
+  CONSTRAINT `FKy__kurslar` FOREIGN KEY (`kurs_id`) REFERENCES `kurslar` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
 
--- kursvt.kullanicilar: ~5 rows (yaklaşık) tablosu için veriler indiriliyor
-INSERT INTO `kullanicilar` (`id`, `kullanici_adi`, `kullanici_sifre`, `rol_id`) VALUES
-	(1, 'ayildirim', '12345', 2),
-	(2, 'root', '5678', 1),
-	(3, 'msimsek', 'xyz', 2),
-	(4, 'adurdu', '789', 3),
-	(5, 'tates', '987', 3);
+-- kursvt.kurs_ogrenci: ~1 rows (yaklaşık) tablosu için veriler indiriliyor
+INSERT INTO `kurs_ogrenci` (`id`, `kurs_id`, `kursiyer_id`) VALUES
+	(1, 1, 3);
 
 -- tablo yapısı dökülüyor kursvt.kurslar
+DROP TABLE IF EXISTS `kurslar`;
 CREATE TABLE IF NOT EXISTS `kurslar` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `kursun_adi` varchar(500) COLLATE utf8mb4_turkish_ci NOT NULL DEFAULT '0',
-  `egitmen_id` int NOT NULL DEFAULT (0),
-  PRIMARY KEY (`id`),
-  KEY `FK2` (`egitmen_id`),
-  CONSTRAINT `FK2` FOREIGN KEY (`egitmen_id`) REFERENCES `kullanicilar` (`id`)
+  `kursun_adi` varchar(500) COLLATE utf8mb4_turkish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
 
 -- kursvt.kurslar: ~2 rows (yaklaşık) tablosu için veriler indiriliyor
-INSERT INTO `kurslar` (`id`, `kursun_adi`, `egitmen_id`) VALUES
-	(1, 'PHP Eğitimi', 1),
-	(2, 'ASP Eğitimi', 3);
+INSERT INTO `kurslar` (`id`, `kursun_adi`) VALUES
+	(1, 'PHP Eğitimi'),
+	(2, 'ASP Eğitimi');
 
 -- tablo yapısı dökülüyor kursvt.roller
+DROP TABLE IF EXISTS `roller`;
 CREATE TABLE IF NOT EXISTS `roller` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `rol_adi` varchar(100) COLLATE utf8mb4_turkish_ci DEFAULT NULL,
+  `rol_adi` varchar(50) COLLATE utf8mb4_turkish_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
 
 -- kursvt.roller: ~3 rows (yaklaşık) tablosu için veriler indiriliyor
 INSERT INTO `roller` (`id`, `rol_adi`) VALUES
-	(1, 'Admin'),
+	(1, 'Yönetici'),
 	(2, 'Eğitmen'),
 	(3, 'Kursiyer');
 
